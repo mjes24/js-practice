325818a4a2242a46687efd28ca3fb183ee740e3a
Hamza <hamzaelouafir91@gmail.com>
2025-09-27 00:10:19 +0200

    pattern.js

---
+++ Pyramid-generator.js
@@ -0,0 +1,9 @@
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
