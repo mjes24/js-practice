1cf9fae657009d1b11df11c242b5bbead7f09be5
Hamza <hamzaelouafir91@gmail.com>
2025-10-12 20:03:52 +0200

    DOM-Manipulation und Spielsteuerung implementieren

--- Rock-Paper-Scissors-Game/script.js
+++ Rock-Paper-Scissors-Game/script.js
@@ -30,3 +30,39 @@ function getRoundResults(userOption) {
   }
 }
 
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+
+};
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+
+  winnerMsgElement.innerText = "";
+  winnerMsgElement.style.display = "none";
+  roundResultsMsg.innerText = "";
+};
