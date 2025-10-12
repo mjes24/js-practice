2f47cc24cfbb4d80c61d0eef4984c455b6c1a22a
Hamza <hamzaelouafir91@gmail.com>
2025-10-12 19:56:01 +0200

    Punktesystem und Rundenergebnis-Funktion implementieren

--- Rock-Paper-Scissors-Game/script.js
+++ Rock-Paper-Scissors-Game/script.js
@@ -11,3 +11,22 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "Paper" && computer === "Rock")
   );
 }
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+
